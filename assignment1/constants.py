bcaca6a1fbd4730ec52ffd20fefab1303396b44b
rishabhBudhouliya <rb5727@nyu.edu>
2025-09-22 14:45:08 -0400

    add api configuration related constants
    Signed-off-by: rishabhBudhouliya <rb5727@nyu.edu>

---
+++ assignment1/constants.py
@@ -0,0 +1,9 @@
+# API Configurations
+
+GET_LOG_ENTRY = "https://rekor.sigstore.dev/api/v1/log/entries?logIndex={0}"
+
+GET_LOG = "https://rekor.sigstore.dev/api/v1/log"
+
+GET_PROOF = "https://rekor.sigstore.dev/api/v1/log/proof?firstSize={0}&lastSize={1}&treeId={2}"
+
+REQUEST_TIMEOUT = 10
