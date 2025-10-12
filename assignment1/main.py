67c01b0732d5c0f42945c00cd7fe69dc3d37599c
rishabhBudhouliya <rb5727@nyu.edu>
2025-10-12 14:53:07 -0400

    applied ruff format and linting changes
    Signed-off-by: rishabhBudhouliya <rb5727@nyu.edu>

--- assignment1/main.py
+++ assignment1/main.py
@@ -1,10 +1,24 @@
 import argparse
-from util import extract_public_key, verify_artifact_signature, validate_artifact_path, validate_log_index, validate_root_hash, validate_tree_size, get_user_auth
-from merkle_proof import DefaultHasher, verify_consistency, verify_inclusion, compute_leaf_hash
+from util import (
+    extract_public_key,
+    verify_artifact_signature,
+    validate_artifact_path,
+    validate_log_index,
+    validate_root_hash,
+    validate_tree_size,
+    get_user_auth,
+)
+from merkle_proof import (
+    DefaultHasher,
+    verify_consistency,
+    verify_inclusion,
+    compute_leaf_hash,
+)
 from constants import GET_LOG, GET_LOG_ENTRY, GET_PROOF, REQUEST_TIMEOUT
 
 import requests
 import json
+
 
 def get_log_entry(log_index, debug=False):
     response = requests.get(GET_LOG_ENTRY.format(log_index), timeout=REQUEST_TIMEOUT)
@@ -12,14 +26,15 @@ def get_log_entry(log_index, debug=False):
     data = response.json()
     try:
         uuid = list(data.keys())[0]
-        log_index = data[uuid]['logIndex']
-        if not isinstance(log_index, int) or log_index <0:
+        log_index = data[uuid]["logIndex"]
+        if not isinstance(log_index, int) or log_index < 0:
             raise ValueError(f"Log index should be a non-negative integer, {log_index}")
-        if not data[uuid]['verification']['inclusionProof']:
+        if not data[uuid]["verification"]["inclusionProof"]:
             raise ValueError(f"Given log index: {log_index} is invalid")
         return data
     except ValueError as e:
         raise e
+
 
 def get_verification_proof(log_entry):
     # forgoeing the cost of additional network call for log index validation with below call
@@ -27,22 +42,27 @@ def get_verification_proof(log_entry):
         if not log_entry:
             raise ValueError("Log entry can't be empty")
         uuid = list(log_entry.keys())[0]
-        proof = log_entry[uuid]['verification']['inclusionProof']
+        proof = log_entry[uuid]["verification"]["inclusionProof"]
     except Exception as e:
         raise e
     return proof
 
+
 def inclusion(log_index, artifact_filepath, debug=False):
     try:
         if not validate_log_index(log_index, debug):
-            raise ValueError(f"Inclusion failed: Unable to validate log index")
+            raise ValueError(
+                f"Inclusion failed: Unable to validate log index: {log_index}"
+            )
         validate_artifact_path(artifact_filepath)
         if debug:
             print(f"Given log index validated: {log_index}")
             print(f"Given artifact path validated: {artifact_filepath}")
         data = get_log_entry(log_index)
         if not data:
-            raise ValueError(f"Log entry missing/invalid: {data}")
+            raise ValueError(
+                f"Log entrysource venv/bin/activate missing/invalid: {data}"
+            )
         signature, public_cert = get_user_auth(data)
         public_key = extract_public_key(public_cert)
 
@@ -51,45 +71,49 @@ def inclusion(log_index, artifact_filepath, debug=False):
         proof = get_verification_proof(data)
         if not proof:
             raise ValueError(f"Verification proof not found/invalid: {proof}")
-        index = proof['logIndex']
-        root_hash = proof['rootHash']
-        tree_size = proof['treeSize']
-        hashes = list(proof['hashes'])
+        index = proof["logIndex"]
+        root_hash = proof["rootHash"]
+        tree_size = proof["treeSize"]
+        hashes = list(proof["hashes"])
         uuid = list(data.keys())[0]
-        leaf_hash = compute_leaf_hash(data[uuid]['body'].encode('utf-8'))
+        leaf_hash = compute_leaf_hash(data[uuid]["body"].encode("utf-8"))
 
-        verify_inclusion(DefaultHasher, index, tree_size, leaf_hash, hashes, root_hash, debug)
+        verify_inclusion(
+            DefaultHasher, index, tree_size, leaf_hash, hashes, root_hash, debug
+        )
     except ValueError as e:
         print(f"Validation error: {e}")
         return False
     except Exception as e:
         print(f"Inclusion verification failed due to: {e}")
         return False
-    
-    print(f"Signature is valid.\nOffline root hash calculation for inclusion verified.")
+
+    print("Signature is valid.\nOffline root hash calculation for inclusion verified.")
     return True
+
 
 def get_latest_checkpoint(debug=False):
     response = requests.get(GET_LOG, timeout=REQUEST_TIMEOUT)
     response.raise_for_status()
     data = response.json()
     if debug:
-        with open('checkpoint.json', 'w') as f:
+        with open("checkpoint.json", "w") as f:
             json.dump(data, f, indent=4)
     return data
+
 
 def consistency(prev_checkpoint, debug=False):
     # verify that prev checkpoint is not empty
     # get_latest_checkpoint()
     try:
-        latest_checkpoint =  get_latest_checkpoint()
-        tree_id = prev_checkpoint['treeID']
-        tree_size = prev_checkpoint['treeSize']
-        root_hash = prev_checkpoint['rootHash']
+        latest_checkpoint = get_latest_checkpoint()
+        tree_id = prev_checkpoint["treeID"]
+        tree_size = prev_checkpoint["treeSize"]
+        root_hash = prev_checkpoint["rootHash"]
 
         size_1 = tree_size
-        size_2 = int(latest_checkpoint['treeSize'])
-        latest_root_hash = latest_checkpoint['rootHash']
+        size_2 = int(latest_checkpoint["treeSize"])
+        latest_root_hash = latest_checkpoint["rootHash"]
 
         if not validate_tree_size(size_1):
             raise ValueError(f"Invalid old checkpoint tree size: {size_1}")
@@ -100,42 +124,71 @@ def consistency(prev_checkpoint, debug=False):
         if not validate_root_hash(latest_root_hash):
             raise ValueError(f"Invalid latest checkpoint root hash: {latest_root_hash}")
 
-        response = requests.get(GET_PROOF.format(size_1, size_2, tree_id), timeout=REQUEST_TIMEOUT)
+        response = requests.get(
+            GET_PROOF.format(size_1, size_2, tree_id), timeout=REQUEST_TIMEOUT
+        )
         response.raise_for_status()
-        verify_consistency(DefaultHasher, size_1, size_2, response.json()['hashes'], root_hash, latest_root_hash)
+        verify_consistency(
+            DefaultHasher,
+            size_1,
+            size_2,
+            response.json()["hashes"],
+            root_hash,
+            latest_root_hash,
+        )
 
     except Exception as e:
         print(f"Failed consistency check: {e}")
         return False
-    
+
     print("Consistency verification successful")
     return True
+
 
 def main():
     debug = False
     parser = argparse.ArgumentParser(description="Rekor Verifier")
-    parser.add_argument('-d', '--debug', help='Debug mode',
-                        required=False, action='store_true') # Default false
-    parser.add_argument('-c', '--checkpoint', help='Obtain latest checkpoint\
-                        from Rekor Server public instance',
-                        required=False, action='store_true')
-    parser.add_argument('--inclusion', help='Verify inclusion of an\
+    parser.add_argument(
+        "-d", "--debug", help="Debug mode", required=False, action="store_true"
+    )  # Default false
+    parser.add_argument(
+        "-c",
+        "--checkpoint",
+        help="Obtain latest checkpoint\
+                        from Rekor Server public instance",
+        required=False,
+        action="store_true",
+    )
+    parser.add_argument(
+        "--inclusion",
+        help="Verify inclusion of an\
                         entry in the Rekor Transparency Log using log index\
                         and artifact filename.\
-                        Usage: --inclusion 126574567',
-                        required=False, type=int)
-    parser.add_argument('--artifact', help='Artifact filepath for verifying\
-                        signature',
-                        required=False)
-    parser.add_argument('--consistency', help='Verify consistency of a given\
-                        checkpoint with the latest checkpoint.',
-                        action='store_true')
-    parser.add_argument('--tree-id', help='Tree ID for consistency proof',
-                        required=False)
-    parser.add_argument('--tree-size', help='Tree size for consistency proof',
-                        required=False, type=int)
-    parser.add_argument('--root-hash', help='Root hash for consistency proof',
-                        required=False)
+                        Usage: --inclusion 126574567",
+        required=False,
+        type=int,
+    )
+    parser.add_argument(
+        "--artifact",
+        help="Artifact filepath for verifying\
+                        signature",
+        required=False,
+    )
+    parser.add_argument(
+        "--consistency",
+        help="Verify consistency of a given\
+                        checkpoint with the latest checkpoint.",
+        action="store_true",
+    )
+    parser.add_argument(
+        "--tree-id", help="Tree ID for consistency proof", required=False
+    )
+    parser.add_argument(
+        "--tree-size", help="Tree size for consistency proof", required=False, type=int
+    )
+    parser.add_argument(
+        "--root-hash", help="Root hash for consistency proof", required=False
+    )
     args = parser.parse_args()
     if args.debug:
         debug = True
@@ -167,6 +220,7 @@ def main():
         prev_checkpoint["rootHash"] = args.root_hash
 
         consistency(prev_checkpoint, debug)
+
 
 if __name__ == "__main__":
     main()
--- assignment1/util.py
+++ assignment1/util.py
@@ -14,37 +14,39 @@ import os
 import re
 from constants import GET_LOG_ENTRY, REQUEST_TIMEOUT
 
+
 # extracts and returns public key from a given cert (in pem format)
 def extract_public_key(cert):
-# read the certificate
-#    with open("cert.pem", "rb") as cert_file:
-#        cert_data = cert_file.read()
+    # read the certificate
+    #    with open("cert.pem", "rb") as cert_file:
+    #        cert_data = cert_file.read()
 
-# load the certificate
+    # load the certificate
     certificate = x509.load_pem_x509_certificate(cert, default_backend())
 
-# extract the public key
+    # extract the public key
     public_key = certificate.public_key()
 
-# save the public key to a PEM file
-#    with open("cert_public.pem", "wb") as pub_key_file:
-#        pub_key_file.write(public_key.public_bytes(
-#            encoding=serialization.Encoding.PEM,
-#            format=serialization.PublicFormat.SubjectPublicKeyInfo
-#        ))
+    # save the public key to a PEM file
+    #    with open("cert_public.pem", "wb") as pub_key_file:
+    #        pub_key_file.write(public_key.public_bytes(
+    #            encoding=serialization.Encoding.PEM,
+    #            format=serialization.PublicFormat.SubjectPublicKeyInfo
+    #        ))
     pem_public_key = public_key.public_bytes(
         encoding=serialization.Encoding.PEM,
-        format=serialization.PublicFormat.SubjectPublicKeyInfo
+        format=serialization.PublicFormat.SubjectPublicKeyInfo,
     )
 
     return pem_public_key
+
 
 def verify_artifact_signature(signature, public_key, artifact_filename):
     # load the public key
     # with open("cert_public.pem", "rb") as pub_key_file:
     #    public_key = load_pem_public_key(pub_key_file.read())
 
-        # load the signature
+    # load the signature
     #    with open("hello.sig", "rb") as sig_file:
     #        signature = sig_file.read()
 
@@ -55,23 +57,22 @@ def verify_artifact_signature(signature, public_key, artifact_filename):
 
     # verify the signature
     try:
-        public_key.verify(
-            signature,
-            data,
-            ec.ECDSA(hashes.SHA256())
-        )
+        public_key.verify(signature, data, ec.ECDSA(hashes.SHA256()))
     except InvalidSignature as e:
-        print("Signature is invalid")
+        print("Signature is invalid", e)
         raise ValueError("Signature is invalid")
     except Exception as e:
         print("Exception in verifying artifact signature:", e)
         raise e
 
+
 def validate_log_index(log_index, debug):
-    if not isinstance(log_index, int) or log_index <0:
+    if not isinstance(log_index, int) or log_index < 0:
         raise ValueError(f"Log index should be a non-negative integer, {log_index}")
     try:
-        response = requests.get(GET_LOG_ENTRY.format(log_index), timeout=REQUEST_TIMEOUT)
+        response = requests.get(
+            GET_LOG_ENTRY.format(log_index), timeout=REQUEST_TIMEOUT
+        )
         response.raise_for_status()
     except requests.HTTPError as e:
         if e.response.status_code == 404:
@@ -86,18 +87,20 @@ def validate_log_index(log_index, debug):
 
     return True
 
+
 # derive signature and public key from given log entry
 def get_user_auth(log_entry):
     uuid = list(log_entry.keys())[0]
-    encoded_bytes = log_entry[uuid]['body'].encode('utf-8')
+    encoded_bytes = log_entry[uuid]["body"].encode("utf-8")
     decoded_bytes = base64.b64decode(encoded_bytes)
-    decoded_string = decoded_bytes.decode('utf-8')
+    decoded_string = decoded_bytes.decode("utf-8")
     body = json.loads(decoded_string)
-    signature = body['spec']['signature']['content']
-    public_cert = body['spec']['signature']['publicKey']['content']
-    decoded_signature = base64.b64decode(signature.encode('utf-8'))
-    decoded_public_cert = base64.b64decode(public_cert.encode('utf-8'))
+    signature = body["spec"]["signature"]["content"]
+    public_cert = body["spec"]["signature"]["publicKey"]["content"]
+    decoded_signature = base64.b64decode(signature.encode("utf-8"))
+    decoded_public_cert = base64.b64decode(public_cert.encode("utf-8"))
     return decoded_signature, decoded_public_cert
+
 
 def validate_artifact_path(artifact_path):
     if not artifact_path:
@@ -106,15 +109,17 @@ def validate_artifact_path(artifact_path):
         raise ValueError(f"Given artifact path: {artifact_path} doesn't exist")
     return True
 
+
 def validate_tree_size(tree_size):
-    if not isinstance(tree_size, int) or tree_size <0:
+    if not isinstance(tree_size, int) or tree_size < 0:
         raise ValueError(f"Tree size should be a non-negative integer, {tree_size}")
     return True
+
 
 # Used ChatGPT to generate this function
 def validate_root_hash(hash):
     if not isinstance(hash, str):
         return False
-    
+
     # Check for 64 hexadecimal characters, case-insensitive
     return bool(re.match(r"^[0-9a-fA-F]{64}$", hash))
